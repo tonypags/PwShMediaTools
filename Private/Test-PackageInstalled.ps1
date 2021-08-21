6f083670873126e2d8d7b2ff5c4fa9f792a34342
Tony Pagliaro <tony@pagliaro.co>
2021-08-21 00:01:32 -0400

    start writing test function

--- Private/Test-PackageInstalled.ps1
+++ Private/Test-PackageInstalled.ps1
@@ -4,30 +4,100 @@ function Test-PackageInstalled {
         # Name of the package to check if installed
         [Parameter(Mandatory, Position=0)]
         [string[]]
-        $Test
+        $Package
     )
 
-    foreach ($t in $Test) {
+    foreach ($app in $Package) {
 
-        if ($IsMacOS) {
-            [PSCustomObject]@{
-                Package   = $t.ToLower()
-                Installed = ((brew list) -contains ($t.ToLower()))
-            }
-        } elseif ($IsLinux) {
-            [PSCustomObject]@{
-                Package   = $t.ToLower()
-                Installed = (
-                    [string]::IsNullOrWhiteSpace(
-                        ((apt list | grep ^ffmpeg) -match ($t.ToLower()))
+        switch -Regex ($app) {
+
+            # First handle any special cases
+
+            '^brew$|^homebrew$'  {
+
+                [PSCustomObject]@{
+                    Package   = 'homebrew'
+                    Installed = [bool]((brew config) -like 'HOMEBREW_VERSION*')
+                    Version = [version](
+                        ((brew config) -like 'HOMEBREW_VERSION*') -replace '^HOMEBREW_VERSION:\s'
                     )
-                )
+
+                }
+ 
             }
-            
-        } else {
-            throw "Unhandled OS: "
-        }
+
+            '^apt$|^apt-get$'    {
+
+                $resp = (apt --version)
+
+                [PSCustomObject]@{
+                    Package   = 'apt'
+                    Installed = (-not [string]::IsNullOrWhiteSpace($resp))
+                    Version = [version](
+                        $resp -replace '^apt\s' -replace '/s\(.+\)$'
+                    )
+                }
+                
+            }
+
+            '^yum$'    {
+
+                $resp = (yum --version)[0]
+
+                [PSCustomObject]@{
+                    Package   = 'yum'
+                    Installed = (-not [string]::IsNullOrWhiteSpace($resp))
+                    Version = [version]$resp
+                }
+
+            }
+ 
+            # Then everything that is a 1:1 string
+
+            Default {
+
+                if ($IsMacOS) {
+                    [PSCustomObject]@{
+                        Package   = $app.ToLower()
+                        Installed = ((brew list) -contains ($app.ToLower()))
+                        Version = [version]([regex]::Match(
+                            (  brew list ( $app.ToLower() )  ), '([\d\.]+)'
+                        ).Groups[1].Value)
+                    }
+                } elseif ($IsLinux) {
+
+                    $aptORyum = Test-PackageInstalled -Package 'apt','yum' |
+                        Where-Object {$_.Installed} |
+                        Select-Object -ExpandProperty 'Package'
+
+                    $resp = if ($aptORyum -eq 'apt') {
+
+                    } elseif ($aptORyum -eq 'yum') {
+
+                    } else {
+                        throw "Unhandled package manager: $($app)"
+                    }
+                    
+                    [PSCustomObject]@{
+                        Package   = $app.ToLower()
+                        Installed = (
+                            [string]::IsNullOrWhiteSpace(
+                                ((apt list | grep ^ffmpeg) -match ($app.ToLower()))
+                            )
+                        )
+                        Version = [version](
+                            (apt --version) -replace '^apt\s' -replace '/s\(.+\)$'
+                        )
+                    }
+                    
+                } else {
+                    Write-Error "Unhandled OS: $(uname)" -ea 'Continue'
+                }
+
+            }#END: Default {}
+
+        }#END: switch -Regex ($app) {}
     
-    }
+    }#END: foreach ($app in $Test) {}
 
 }#END: function Test-PreRequisite {}
