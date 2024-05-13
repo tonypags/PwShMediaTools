a76907f03b68575ed933f4f8b194675b7e2cc0a0
Tony Pagliaro <tony@pagliaro.co>
2024-05-12 21:40:21 -0400

    Fixing bugs, escape strings Syncing features, logging, etc. Update logic to match 2 scripts after testing rename some variables update bitrate prop names function changed underneath use ps7 switch in some places Fix while loop condition pattern Handle divide by zero issue Add parameter to discard video

--- Scripts/top-10-flac-folders.ps1
+++ Scripts/top-10-flac-folders.ps1
@@ -5,12 +5,18 @@
 Converts FLAC albums to Mpeg4 Audio. Confirms m4a file before sending flac to the trash.
 #>
 
-ipmo PwshMediaTools -ea Stop
+ipmo PwshMediaTools -force -ea Stop -wa 0
 
 # Specify the working folders
+$log_folder    = '~/log/top-ten-flac-folders'
 $music_folder  = "/garage/media/Music"
 $trash_folder  = '/garage/transients/trash'
 $chmod_recurse = "$music_folder/recurse_chmod_Music.sh" # post-script to normalize owner/mode
+
+# LOGGING
+$dt = {(Get-Date).ToString('yyyyMMddTHHmmss.fff')}
+$msg = "Starting script..."
+echo "[INFO] [$(icm $dt)] $msg" >> $log_folder
 
 # Find all directories containing *.flac files
 $flacFiles = Get-ChildItem $music_folder -Include '*.flac' -Recurse
@@ -20,12 +26,11 @@ $dirGroup = $flacFiles | Group-Object Directory -NoElement
 $i = 0
 $FolderSizes = [System.Collections.ArrayList]@()
 $albumsToRename = [System.Collections.ArrayList]@()
+Write-Host 'inspecting album sizes..' -NoNewLine
 foreach ($album in $dirGroup) {
 
     $i++
-    # if ($album.Name -match '\[') {$albumsToRename.Add($album) ; continue}
-    $theseSongs = Get-ChildItem "$(Escape-Path -Path $album.Name)" -Recurse | ? Extension -eq '.flac'
-    # Wait-Debugger;return # temp debug
+    $theseSongs = Get-ChildItem "$(Escape-Path -Path $album.Name -PS7Item)" -Recurse | ? Extension -eq '.flac'
     $thisMeasure = $theseSongs | Measure-Object -Property Length -Sum
     $thisSize = $thisMeasure | % Sum
     [void] $FolderSizes.Add(
@@ -38,8 +43,10 @@ foreach ($album in $dirGroup) {
         }
     )
     # break # temp debug
+    Write-Host '.' -NoNewLine
     Start-Sleep -milli 100
 }
+Write-Host ' DONE!' -f Green
 
 if ($albumsToRename) {
     Write-Host -f Red "Albums to rename:`n$(
@@ -49,10 +56,9 @@ if ($albumsToRename) {
 
 # Display the top 10 items and ask to exclude any
 $topTen = $FolderSizes|? bytes -gt 0 |sort-object bytes -desc | select-object -first 10
-if ($null -eq $topTen) {Write-Host 'No Flac Files found :)' -f Green; return}
+if ($null -eq $topTen) {Write-Host 'No Flac Files found :)' -f Green -b DarkYellow; return}
 
 do {
-    clear-Host
     Write-Host "`nConverting these FLAC files to M4A files? (then touch & move FLACs to $trash_folder/)"
     $topTen | sort-object '#' | Ft -a
     [int]$choice = Read-Host "Exclude any of these? (enter # to exclude, Enter/0 to continue)"
@@ -60,7 +66,6 @@ do {
 
 } while ($choice -ne 0)
 
-clear-Host
 $topTen | sort-object '#' | Ft -a
 $yn = Read-Host 'Are you sure you want to convert these files and trash them (y/N)?'
 if ($yn -notlike '*y*') {return}
@@ -70,12 +75,16 @@ Push-Location
 # Execute loop on all album folders
 $totalAlbums = @($topTen).Count
 $iAlbum = 0
+$totalGbSaved = 0
 foreach ($item in $topTen) {
     $iAlbum++
+    $artist = Split-Path (Split-Path $item.Path -Parent) -Leaf
 
-    cd "$(Escape-Path -Path $item.Path)"
+    $this_wdir = Escape-Path -Path $item.Path -PS7Item
+    echo "[INFO] [$(icm $dt)] cd `"$($this_wdir)`"" >> $log_folder
+    Set-Location "$this_wdir"
     Write-Host "$('=' * $host.UI.RawUI.WindowSize.Width)`n
-    `nConverting Album" -NoNewLine ; Write-Host " [$($iAlbum) of $($totalAlbums)]: $($item.Album)`n" -f Green
+    `nConverting $($artist) Album " -NoNewLine ; Write-Host "[$($iAlbum) of $($totalAlbums)]: $($item.Album)`n" -f Green
 
     # for each item, convert it, confirm it, touch old item, move it to trash
     $flacs = Get-ChildItem "*.flac"
@@ -83,38 +92,67 @@ foreach ($item in $topTen) {
     $iSong = 0
     foreach ($song in $flacs) {
         $iSong++
+        $thisLabel = "$($artist) / $($item.Album) / $($song.Name)"
 
         # convert it
-        $newName = "$(Escape-Path -Path $song.Basename).m4a"
-        $cmd = "ffmpeg -i `"$(Escape-Path -Path $song.Name)`" -v quiet -stats -vsync 0 `"$($newName -replace '"','`"')`""
+        $oldName = "$(Escape-Path -Path $song.Name)"
+        $oldName7 = "$(Escape-Path -Path $song.Name -PS7Item)"
+        $newName = "$(Use-StraightQuotes -String $song.Basename).m4a"
+        $newNameEsc = "$(Escape-Path $newName)"
+        $newName7 = "$(Escape-Path $newName -PS7Item)"
+        $cmd = "ffmpeg -i `"$oldName`" -v quiet -stats -vsync 0 -vn `"$($newNameEsc)`""
         Write-Host "`nConverting Song " -NoNewLine ; Write-Host " [$($iSong) of $($totalSongs)]: $($song.Name)" -f Cyan
-        Write-Host "Running this: $cmd `n"
+        echo "[INFO] [$(icm $dt)] $cmd" >> $log_folder
+        #Wait-Debugger;Pop-Location;return # temp debug
         Invoke-Expression $cmd
 
+        $oldMeta = Get-MediaInfo $song.Name | % General
+        $newMeta = Get-MediaInfo $newName | % General
+        $newItem = Get-Item $newName
+        # Wait-Debugger
+
+        $sizeDiff = $song.Length - $newItem.Length
+        $sizeDiffMB = [math]::Round(($sizeDiff/1MB),0)
+        $sizeDiffGB = [math]::Round(($sizeDiff/1GB),2)
+        $totalGbSaved += $sizeDiffGB
+        $sizePct = ($sizeDiff / $song.Length * 100) -as [int]
+        Write-Host "File size lowered" -NoNewLine ; Write-Host " $($sizePct)% or $($sizeDiffMB)MB" -f Yellow
+
         # confirm it
-        $oldMeta = Get-MediaInfo "$(Escape-Path -Path $song.Name)" | % General
-        $newMeta = Get-MediaInfo "$(Escape-Path -Path $newName)" | % General
         $durDiff = $oldMeta.Duration.TotalSeconds - $newMeta.Duration.TotalSeconds
         $durDiffSeconds = [math]::Abs($durDiff)
+        if ($null -eq $newMeta) {
+            $msg = "Conversion Failed! $($thisLabel)"
+            Write-Host $msg -f Red
+            echo "[ERROR] [$(icm $dt)] $msg" >> $log_folder
 
-        if ($durDiffSeconds -le 1) {
+        } elseif ($durDiffSeconds -le 1) {
 
             # touch and trash it
-            touch "$(Escape-Path -Path $song.Name)"
-            mv "$(Escape-Path -Path $song.Name)" "$trash_folder"
+            touch "$($song.Name)"
+            mv "$($song.Name)" "$trash_folder"
             Write-Host "Trashed file:" -NoNewLine ; Write-Host " $($song.Name)" -f Yellow
+            # Wait-Debugger
 
         } else {
 
-            Write-Host "Duration mismatch! $($item.Album) / $($song.Name)" -f Red
+            $msg = "Duration mismatch! $thisLabel"
+            Write-Host $msg -f Red
+            echo "[ERROR] [$(icm $dt)] $msg" >> $log_folder
         }
         # break # temp debug
     }
     # break # temp debug
 }
+$msg1 = "Total space saved: "
+$msg2 = "$($totalGbSaved)GB"
+Write-Host -NoNewLine $msg1; Write-Host $msg2 -f Green
+echo "[INFO] [$(icm $dt)] $msg1 $msg2" >> $log_folder
 
 Pop-Location
 
+Write-Host ""
+Write-Host "Log file available here: $log_folder"
 Write-Host ""
 Write-Host "please run this command now:"
 Write-Host ""
--- Scripts/top-tv-folders.ps1
+++ Scripts/top-tv-folders.ps1
@@ -3,24 +3,48 @@
 <#
 .SYNOPSIS
 Converts high bitrate videos of any type to smaller x264 MP4. Confirms mp4 file before sending original file to the trash.
+.NOTES
+Tracking good CRFs to use for 750MB results
+4.5GB - 28
+4.0GB - 27
+3.5GB - 27
+
+Try a bash script using this one-liner:
+find ./ -name "*.mkv" -exec sh -c 'ffmpeg -i "$1" -crf 27 "$(basename "$1" mkv).mp4"' _ {} \;
+find ./ -name "*.flac" -exec sh -c 'ffmpeg -i "$1" "$(basename "$1" flac).m4a"' _ {} \;
 #>
 param(
     # higher CRF = smaller file
-    [int]$crf = 28,
+    [int]$crf = 27,
 
     # how big does a file have to be to get converted
-    [int64]$minSize = 4.5GB,
+    [int64]$minSize = 4.0GB,
 
     # how many TV shows to show the enduser at once
     $maxItems = 15
 )
 
-ipmo PwshMediaTools -ea Stop
+ipmo PwshMediaTools -ea Stop -wa 0
 
 # Specify the working folders
-$base_folder  = "/garage/media/TV"
+$log_folder    = '~/log/top-tv-folders'
+$base_folder   = "/garage/media/TV"
 $trash_folder  = '/garage/transients/trash'
 $chmod_recurse = "/garage/media/recurse_chmod_media.sh" # post-script to normalize owner/mode
+
+# LOGGING
+$dt = {(Get-Date).ToString('yyyyMMddTHHmmss.fff')}
+$msg = "Starting script..."
+echo "[INFO] [$(icm $dt)] $msg" >> $log_folder
+$msg = "CRF: $($crf)"
+Write-Host $msg
+echo "[INFO] [$(icm $dt)] $msg" >> $log_folder
+$msg = "minSize: $($minSize) ($($minSize/1GB)GB)"
+Write-Host $msg
+echo "[INFO] [$(icm $dt)] $msg" >> $log_folder
+$msg = "maxItems: $($maxItems)"
+Write-Host $msg
+echo "[INFO] [$(icm $dt)] $msg" >> $log_folder
 
 # Find top directories by size
 $i = 0
@@ -28,20 +52,21 @@ $allFolders = Get-ChildItem $base_folder -Directory
 Write-Host 'inspecting show sizes..' -NoNewLine
 $sizedFolders = foreach ($show in $allFolders) {
     $i++
-    $kids = Get-ChildItem (Escape-Path -Path $show.FullName) -File -Recurse | Where-Object Length -gt $minSize
-    if ($null -eq $kids) {continue}
-    $size = $kids | Measure-Object -Property Length -Sum | % Sum
+    $lrgEpisodes = Get-ChildItem $show.FullName -File -Recurse | Where-Object Length -gt $minSize
+    # Wait-Debugger;return # temp debug
+    if ($null -eq $lrgEpisodes) {continue}
+    $size = $lrgEpisodes | Measure-Object -Property Length -Sum | % Sum
     [PsCustomObject]@{
         '#' = $i
         Show = $show.Name
         SizeGB = [math]::Round(($size/1GB),0) -as [int]
-        Files = @($kids).Count
+        Files = @($lrgEpisodes).Count
         FullName = $show.FullName
     }
     Write-Host '.' -NoNewLine
     Start-Sleep -milli 10
 }
-Write-Host 'DONE!' -f Green
+Write-Host ' DONE!' -f Green
 
 $bigFolders = $sizedFolders | Sort-Object SizeGB -Desc | Select-Object -First $maxItems
 if ($null -eq $bigFolders) {Write-Host 'No Files found :(' -f Red; return}
@@ -54,10 +79,11 @@ do {
     $choice = $bigFolders | ? '#' -eq $entry
     $yn = Read-Host "Compress $($choice.Files) episodes of $($choice.Show) (y/N)?"
 
-} while ($yn -notmatch 'y*')
+} while ($yn -notmatch 'y.*')
 
-$kids = Get-ChildItem (Escape-Path -Path $choice.FullName) -Recurse | Where-Object Length -gt $minSize
-Write-Host ($kids | Ft -a | out-string)
+$lrgEpisodes = Get-ChildItem ($choice.FullName) -Recurse | Where-Object Length -gt $minSize
+Write-Host ($lrgEpisodes | Ft -a | out-string)
+# Wait-Debugger
 $yn = Read-Host "Are you sure you want to compress these files @CRF $crf and trash them (y/N)?"
 if ($yn -notlike 'y*') {return}
 
@@ -66,53 +92,84 @@ Push-Location
 # for each item, convert it, confirm it, touch old item, move it to trash
 $i = 0
 $totalGbSaved = 0
-foreach ($item in $kids) {
+foreach ($item in $lrgEpisodes) {
     $i++
+    $showLabel = Split-Path (Split-Path $item.FullName -Parent) -Leaf
+    if ($showLabel -like 'Season*') {
+        $season = $showLabel
+        $showLabel = Split-Path (Split-Path (Split-Path $item.FullName -Parent) -Parent) -Leaf
+    } else {
+        $season = ''
+    }
+    $thisLabel = "$showLabel / $season / $($item.Basename)"
 
-    Write-Host "Converting Episode" -NoNewLine ; Write-Host " [$($i) of $($choice.Files)]: $($item.Name)`n" -f Green
-    cd "$(Escape-Path -Path $item.Directory)"
+    Write-Host "Converting Episode " -NoNewLine ; Write-Host "[$($i) of $($choice.Files)]: $($item.Name)`n" -f Green
+    # $this_wdir = Escape-Path -Path $item.Directory
+    $this_wdir = $item.Directory
+    echo "[INFO] [$(icm $dt)] $thisLabel" >> $log_folder
+    cd "$this_wdir"
 
     # convert it
-    $newName = "$(Escape-Path -Path $item.Basename).mp4"
-    $cmd = "ffmpeg -i `"$(Escape-Path -Path $item.Name)`" -v quiet -stats -crf $crf `"$(Escape-Path -Path $newName)`""
-    Write-Host "Running this: $cmd `n"
+    $oldName = "$(Escape-Path -Path $item.Name)"
+    # $oldName7 = "$(Escape-Path -Path $item.Name -PS7Item)"
+    $newName = "$(Use-StraightQuotes -String $item.Basename).mp4"
+    $newNameEsc = "$(Escape-Path $newName)"
+    # $newName7 = "$(Escape-Path $newName -PS7Item)"
+    $cmd = "ffmpeg -i `"$oldName`" -v quiet -stats -crf $crf `"$($newNameEsc)`""
+    echo "[INFO] [$(icm $dt)] $cmd" >> $log_folder
+    # Wait-Debugger;Pop-Location;return # temp debug
     Invoke-Expression $cmd
 
-    # confirm it
-    $oldMeta = Get-MediaInfo "$(Escape-Path -Path $item.Name)" | % General
-    $newMeta = Get-MediaInfo "$(Escape-Path -Path $newName)" -ea 0 | % General
+    $oldMeta = Get-MediaInfo "$($item.Name)" | % General
+    $newMeta = Get-MediaInfo "$($newName)" | % General
+    $newItem = Get-Item "$($newName)"
+    # Wait-Debugger
 
-    $bitDiff = $oldMeta.'Overall bit rate (kbps)' - $newMeta.'Overall bit rate (kbps)'
-    $bitPct = $bitDiff / $oldMeta.'Overall bit rate (kbps)' * 100 -as [int]
-    Write-Host "Bitrate lowered" -NoNewLine ; Write-Host " $($bitPct)% from $($oldMeta.'Overall bit rate (kbps)')k to $($newMeta.'Overall bit rate (kbps)')k" -f Yellow
+    $bitDiff = $oldMeta.'Overall bit rate (bps)' - $newMeta.'Overall bit rate (bps)'
+    $bitPct = if ($oldMeta.'Overall bit rate (bps)' -gt 0) {
+        $bitDiff / $oldMeta.'Overall bit rate (bps)' * 100 -as [int]
+    } else {'??'}
+    Write-Host "Bitrate lowered" -NoNewLine ; Write-Host " $($bitPct)% from $($oldMeta.'Overall bit rate (bps)'/1kb)k to $($newMeta.'Overall bit rate (bps)'/1kb)k" -f Yellow
 
-    $sizeDiff = $oldMeta.'File size' - $newMeta.'File size'
+    $sizeDiff = $item.length - $newItem.Length
     $sizeDiffGB = [math]::Round(($sizeDiff/1GB),2)
     $totalGbSaved += $sizeDiffGB
-    $sizePct = $sizeDiff / $oldMeta.'File size' * 100 -as [int]
+    $sizePct = $sizeDiff / $item.Length * 100 -as [int]
     Write-Host "File size lowered" -NoNewLine ; Write-Host " $($sizePct)% or $($sizeDiffGB)GB" -f Yellow
 
+    # confirm it
     $durDiff = $oldMeta.Duration.TotalSeconds - $newMeta.Duration.TotalSeconds
     $durDiffSeconds = [math]::Abs($durDiff)
     if ($null -eq $newMeta) {
-        Write-Host "Conversion Failed! $($item.Name)" -f Red
+        
+        $msg = "Conversion Failed! $($thisLabel)"
+        Write-Host $msg -f Red
+        echo "[ERROR] [$(icm $dt)] $msg" >> $log_folder
 
     } elseif ($durDiffSeconds -le 1) {
 
         # touch and trash it
-        touch "$(Escape-Path -Path $item.Name)"
-        mv "$(Escape-Path -Path $item.Name)" "$trash_folder"
+        touch "$($item.Name)"
+        mv "$($item.Name)" "$trash_folder"
         Write-Host "Trashed file:" -NoNewLine ; Write-Host " $($item.Name)" -f Yellow
 
     } else {
 
-        Write-Host "Duration mismatch! $($item.Name)" -f Red
+        $msg = "Duration mismatch! $thisLabel"
+        Write-Host $msg -f Red
+        echo "[ERROR] [$(icm $dt)] $msg" >> $log_folder
     }
+    # break # temp debug
 }
-Write-Host "Total space saved: " -NoNewLine ; Write-Host "$($totalGbSaved)GB" -f Green
+$msg1 = "Total space saved: "
+$msg2 = "$($totalGbSaved)GB"
+Write-Host -NoNewLine $msg1; Write-Host $msg2 -f Green
+echo "[INFO] [$(icm $dt)] $msg1 $msg2" >> $log_folder
 
 Pop-Location
 
+Write-Host ""
+Write-Host "Log file available here: $log_folder"
 Write-Host ""
 Write-Host "please run this command now:"
 Write-Host ""
